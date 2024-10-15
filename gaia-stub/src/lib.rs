bc66ef79c0e36a0a220d753023c4f8415ce4fa6f
shimomura-shunsuke <shunsuke0406.1246.rex@gmail.com>
2024-10-15 16:57:53 +0900

    文字への変換を追加

--- gaia-stub/src/lib.rs
+++ gaia-stub/src/lib.rs
@@ -28,4 +28,32 @@ pub mod cop {
     tonic::include_proto!("cop");
 
     pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("cop_descriptor");
+    pub mod status {
+        pub fn task_status_to_string(task_status: super::CopTaskStatusPattern) -> String {
+            match task_status {
+                super::CopTaskStatusPattern::Pending => "PENDING".to_string(),
+                super::CopTaskStatusPattern::Accepted => "ACCEPTED".to_string(),
+                super::CopTaskStatusPattern::Canceled => "CANCELED".to_string(),
+                super::CopTaskStatusPattern::Failed => "FAILED".to_string(),
+                super::CopTaskStatusPattern::Executed => "EXECUTED".to_string(),
+                super::CopTaskStatusPattern::Lockout => "LOCKOUT".to_string(),
+                super::CopTaskStatusPattern::Timeout => "TIMEOUT".to_string(),
+                super::CopTaskStatusPattern::Rejected => "REJECTED".to_string(),
+            }
+        }
+        
+        pub fn worker_state_to_string(worker_state: super::CopWorkerStatusPattern) -> String {
+            match worker_state {
+                super::CopWorkerStatusPattern::WorkerClcwUnreceived => "CLCW_UNRECEIVED".to_string(),
+                super::CopWorkerStatusPattern::WorkerIdle => "IDLE".to_string(),
+                super::CopWorkerStatusPattern::WorkerActive => "ACTIVE".to_string(),
+                super::CopWorkerStatusPattern::WorkerInitialize => "INITIALIZE".to_string(),
+                super::CopWorkerStatusPattern::WorkerCanceled => "CANCELED".to_string(),
+                super::CopWorkerStatusPattern::WorkerFailed => "FAILED".to_string(),
+                super::CopWorkerStatusPattern::WorkerLockout => "LOCKOUT".to_string(),
+                super::CopWorkerStatusPattern::WorkerTimeout => "TIMEOUT".to_string(),
+                super::CopWorkerStatusPattern::WorkerUnlocking => "UNLOCKING".to_string(),
+            }
+        }
+    }
 }
